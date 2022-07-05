db59c99550a595157d9d3baec3f0a01084b9e90b
wisnieskii <106274579+wisnieskii@users.noreply.github.com>
2022-07-05 19:43:32 -0400

    Formulário com JavaScript

---
+++ script.js
@@ -0,0 +1,67 @@
+const form = document.getElementById("form");
+const usarname = document.getElementById("usarname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const usarnameValue = usarname.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usarnameValue == "") {
+    setErrorFor(usarname, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(usarname);
+  }
+
+  if (emailValue == "") {
+    setErrorFor(email, "O email é obrigatório");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue == "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no minímo 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (passwordConfirmationValue == "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória");
+  } else if (passwordConfirmationValue != passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas não conferem.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
